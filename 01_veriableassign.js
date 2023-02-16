b4efc34b1610e20cf1416a05e43872e7f8efe1ba
pravinpatilw9@gmail.com <pravinpatilw9@gmail.com>
2023-02-16 21:48:15 +0530

    my first JS prg
    my First JS prg

---
+++ 01_veriableassign.js
@@ -0,0 +1,17 @@
+console.log("*****Step-1*****");
+var city ;
+console.log(city);
+
+console.log("My Full Name is: Pravin patil");
+console.log("My Full Name with middle Name is : Pravin Gajanan Patil");
+
+console.log("******Step-2******");
+console.log("I love only JS");
+
+console.log("******Step-3******");
+console.log("My first Name is :pravin");
+console.log("My last Name is :Patil");
+console.log("My Age is :27");
+console.log("My address is :at post Akola ,TQ & dist Akola");
+console.log("My Gender is:Male ");
+console.log("Marital Status :No");
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,9 @@
 console.log("Pravin Patil");
+console.log("27");
+console.log("pune");
+
+var fullName ="Pravin patil";
+console.log(fullName);
+console.log(fullName);
+
+var city;
