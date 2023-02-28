0fb231d8ca21a89281d3348d5673eeb3532d5dea
pravinpatilw9@gmail.com <pravinpatilw9@gmail.com>
2023-02-28 23:45:56 +0530

    string
    and swapping

--- 02_SwapVariableass.js
+++ 02_SwapVariableass.js
@@ -17,14 +17,11 @@ var num3=300;
 
 
 var temp =num1
-num1 =num2
-num2=temp
+var num1 =num2
+var num2=num3
+var num3=temp
 
- var temp =num3
-var num3 = num1
-num1 =temp
-
-
+ 
 
 console.log(num1,num2,num3);
 
---
+++ stringBasicAss.js
@@ -0,0 +1,13 @@
+function stringBasics (){
+    console.log("my dream company is 'Tcs'.");
+         
+
+    var v1="cricket";
+    var v2="dancing";
+    var v3="cooking";
+
+console.log(`my  hobies are  ${v1},${v2},${v3}`);
+console.log(v1.length+v2.length+v3.length);
+    
+}
+stringBasics();
