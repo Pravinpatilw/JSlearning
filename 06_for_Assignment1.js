9488049872462cc1104ab13cca5f02f491960d58
pravinpatilw9@gmail.com <pravinpatilw9@gmail.com>
2023-03-01 22:18:28 +0530

    loop
    loopAss

---
+++ 06_for_Assignment1.js
@@ -0,0 +1,34 @@
+console.log(`Wap to print 5 to 15 by increment 1`);
+
+for (let index = 5; index <=15; index++){
+    console.log(index);   
+}
+
+console.log("Wap to print number from 50 to 40 by decrementing by 1");
+for (let index =50; index >=40; index--){
+    console.log(index);  
+}
+
+console.log("wap to find first 15 odd number ");
+for (let index = 1; index <=15; index =index+2) {
+    console.log(index);
+    
+}
+console.log("wap to find first 10 even number ");
+for (let index = 0; index <=10; index =index+2) {
+    console.log(index);
+}
+
+console.log("wap to 5 table ");
+for (let index = 5; index <=50; index =index+5) {
+    console.log(index);
+}
+console.log("wap to 10 table ");
+for (let index = 10; index <=100; index =index+10) {
+    console.log(index);
+}
+
+console.log("wap to 10 table reverse  ");
+for (let index = 100; index >=10; index =index-10) {
+    console.log(index);
+}
